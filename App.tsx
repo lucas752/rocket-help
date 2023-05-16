b488c497826961175ed02f06ca79c76a5e1b4ab6
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-05-16 16:37:53 -0300

    configurando as fontes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
 import { SignIn } from "./src/screens/Signin";
+import { THEME } from "./src/styles/theme";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
   return (
-    <NativeBaseProvider>
-      <SignIn/>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content" 
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn/> : <Loading/>}
     </NativeBaseProvider>
   );
 }
