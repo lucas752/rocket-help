d7b136197d3ff91e0079970cd2f4a7a7eda277fb
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-05-16 18:58:39 -0300

    salvando informações dos inputs

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { VStack, Heading, Icon, useTheme } from "native-base";
 import Logo from "../assets/logo_primary.svg"
 import { Envelope, Key } from 'phosphor-react-native'
@@ -6,6 +7,13 @@ import { Button } from "../components/Button";
 
 export function SignIn() {
     const {colors} = useTheme();
+
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('')
+
+    function handleSignIn() {
+        console.log(`Senha: ${password} | E-mail: ${name}`)
+    }
 
     return(
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -18,15 +26,17 @@ export function SignIn() {
                 placeholder="E-mail"
                 mb={4}
                 InputLeftElement={< Icon as={<Envelope color={colors.gray[300]}/>} ml={4}/>}
+                onChangeText={setName}
             />
             <Input 
                 placeholder="Senha"
                 InputLeftElement={< Icon as={<Key color={colors.gray[300]}/>} ml={4}/>}
                 secureTextEntry
                 mb={8}
+                onChangeText={setPassword}
             />
 
-            <Button title="Entrar" w="full"/>
+            <Button title="Entrar" w="full" onPress={handleSignIn}/>
         </VStack>
     )
 }
