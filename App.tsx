7211b3984f0279f371b04119ef30a930543cf8f9
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-05-17 16:09:57 -0300

    adicionando tela de registro

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
-import { Home } from "./src/screens/Home";
+import { Register } from "./src/screens/Register";
 import { THEME } from "./src/styles/theme";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from "./src/components/Loading";
@@ -17,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Home/> : <Loading/>}
+      {fontsLoaded ? <Register/> : <Loading/>}
     </NativeBaseProvider>
   );
 }
